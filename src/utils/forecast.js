4f5527f4fe170002a0e0dc247ac86a483fe4c4de
galshultz <galshultz88@gmail.com>
2021-12-26 15:59:37 +0200

    add changes to forecast.js file to include more information

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,19 +1,25 @@
-const request = require("request")
-const geoCode = require("./geocode")
-
+const request = require("request");
+const geoCode = require("./geocode");
 
 const forecast = (lon, lat, callback) => {
-    const url = 'http://api.weatherstack.com/current?access_key=6b38f49182c8ef7963caee8c3743cfd6&query=' + lat + ',' + lon 
-    request({ url, json: true }, (error, { body }) => {
-        if (error) {
-            callback('Unable to connect to location services ', error)
-        } else if (body.error) {
-            callback('Unable to find location', error);
-        } else {
-            const currentResponse = body.current
-            callback(undefined, currentResponse.weather_descriptions[0] + '. It is currently ' + currentResponse.temperature + ' Degrees out, It feels like ' + currentResponse.feelslike)
-        }
-    })
-}
+  const url =
+    "http://api.weatherstack.com/current?access_key=6b38f49182c8ef7963caee8c3743cfd6&query=" +
+    lat +
+    "," +
+    lon;
+  request({ url, json: true }, (error, { body }) => {
+    if (error) {
+      callback("Unable to connect to location services ", error);
+    } else if (body.error) {
+      callback("Unable to find location", error);
+    } else {
+      const currentResponse = body.current;
+      callback(
+        undefined,
+        "Today is " + currentResponse.weather_descriptions[0] + ". It is currently " + currentResponse.temperature +
+        " degrees outside, and it feels like " +currentResponse.feelslike +". Humidity is " +currentResponse.humidity +"%. And the uv index is " +currentResponse.uv_index+".");
+    }
+  });
+};
 
-module.exports = forecast
+module.exports = forecast;
